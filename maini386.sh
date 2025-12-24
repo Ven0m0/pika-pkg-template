22bc9e9d71282a71692ee04a339ce8b31c89e197
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-24 04:40:12 +0100

    Update maini386.sh

--- maini386.sh
+++ maini386.sh
@@ -1,26 +1,16 @@
-#! /bin/bash
-
-set -e
+#!/usr/bin/env bash
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR
+set -e; shopt -s nullglob globstar; IFS=$'\n\t'
 
 VERSION="1.0"
-
-export DEBIAN_FRONTEND="noninteractive"
+export DEBIAN_FRONTEND="noninteractive" LC_ALL=C DPKG_GENSYMBOLS_CHECK_LEVEL=0
 export DEB_BUILD_OPTIONS="nocheck notest terse"
-export DPKG_GENSYMBOLS_CHECK_LEVEL=0
-
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
-
+mkdir -p ./src-pkg-name && cp -rvf ./debian ./src-pkg-name/ && cd ./src-pkg-name/
 # Get build deps
 LOGNAME=root dh_make --createorig -y -l -p src-pkg-name_"$VERSION" || echo "dh-make: Ignoring Last Error"
 apt-get build-dep ./ -y
-
 # Build package
 dpkg-buildpackage --no-sign
-
 # Move the debs to output
-cd ../
-mkdir -p ./output
-mv ./*.deb ./output/
+cd ../ && mkdir -p ./output && mv ./*.deb ./output/
