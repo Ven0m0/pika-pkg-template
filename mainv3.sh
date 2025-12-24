f4cca17adb164e4aede125fbc7955631764f7948
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-24 04:37:00 +0100

    Update mainv3.sh

--- mainv3.sh
+++ mainv3.sh
@@ -1,31 +1,22 @@
-#! /bin/bash
-
+#!/bin/bash
 set -e
 
 VERSION="1.0"
-
-export DEBIAN_FRONTEND="noninteractive"
-export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEBIAN_FRONTEND="noninteractive" LC_ALL=C
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -pipe -fno-semantic-interposition -fdata-sections -ffunction-sections"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -pipe -fno-semantic-interposition -fdata-sections -ffunction-sections"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -pipe -fno-semantic-interposition -fdata-sections -ffunction-sections"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -pipe -fno-semantic-interposition -fdata-sections -ffunction-sections"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32 -pipe -fno-semantic-interposition -fdata-sections -ffunction-sections"
 export DEB_BUILD_OPTIONS="nocheck notest terse"
 export DPKG_GENSYMBOLS_CHECK_LEVEL=0
 
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
-
+mkdir -p ./src-pkg-name && cp -rvf ./debian ./src-pkg-name/ && cd ./src-pkg-name/
 # Get build deps
 LOGNAME=root dh_make --createorig -y -l -p src-pkg-name_"$VERSION" || echo "dh-make: Ignoring Last Error"
 apt-get build-dep ./ -y
-
 # Build package
 dpkg-buildpackage --no-sign
-
 # Move the debs to output
-cd ../
-mkdir -p ./output
-mv ./*.deb ./output/
+cd ../ && mkdir -p ./output && mv ./*.deb ./output/
